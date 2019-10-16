94a93ab836934c790e75c0e3ba9b8bed52c00e55
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-16 14:56:49 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,7 +2,8 @@ class EmailAddressParser
   attr_accessor :email_addresses 
   
   def initialize(emails)
-    
+    self.email_addresses = emails 
+  end
   
   def parse
     emails = emails.split(/[\s,]/)
