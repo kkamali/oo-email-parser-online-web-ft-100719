54130276a044381eae1cfdef9afd555bc29bfb53
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-16 14:56:38 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,8 +1,10 @@
 class EmailAddressParser
   attr_accessor :email_addresses 
   
-  def parse(emails)
-    @email_addresses = []
+  def initialize(emails)
+    
+  
+  def parse
     emails = emails.split(/[\s,]/)
     emails.each{|email| @email_addresses << email}
     @email_addresses.uniq
